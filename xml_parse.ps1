5200c4bd47ccbd76c5e2427dc3f628435863acd7
hollyRock <thoroiwa@gmail.com>
2017-08-19 15:24:42 +0900

    beta1
    add parser

---
+++ xml_parse.ps1
@@ -0,0 +1,36 @@
+﻿#  Battery XML Parser
+###############################
+
+Write-host "Parse xml file.."
+$out_csv_file = ".\out.csv"
+
+
+# Remove outputfile if it existed
+If (Test-Path $out_csv_file){
+    Remove-Item $out_csv_file
+}
+
+# Load XML file
+#Get-ChildItem .\logs\*.* -include *.xml
+$files = Get-ChildItem .\logs\*.* -Filter *.xml
+Foreach($file_temp in $files){
+
+    $xmlDoc=[xml](Get-Content $file_temp)
+
+    # Output retrieve data to csv format
+    foreach ($name in $xmlDoc.payload.'session-tracking-info'.attribute.name){
+        Write-Output $name ", " | Out-File ${out_csv_file} -NoNewline -Append -Encoding ascii
+    }
+    foreach ($name in $xmlDoc.payload.element.attribute.name){
+        Write-Output $name ", " | Out-File ${out_csv_file} -NoNewline -Append -Encoding ascii
+    }
+    Write-Output "EOD" | Out-File ${out_csv_file} -Append -Encoding ascii
+
+    foreach ($value in $xmlDoc.payload.'session-tracking-info'.attribute.value){
+        Write-Output $value ", " | Out-File ${out_csv_file} -NoNewline -Append -Encoding ascii
+    }
+    foreach ($value in $xmlDoc.payload.element.attribute.value){
+        Write-Output $value ", " | Out-File ${out_csv_file} -NoNewline -Append -Encoding ascii
+    }
+    Write-Output "EOD" | Out-File ${out_csv_file} -Append -Encoding ascii
+}
